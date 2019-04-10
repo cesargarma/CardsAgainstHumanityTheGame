7ce1948c845c9b6f3d8dc2e8d2677eb1393aad3e
Golthork <46814986+cesargarma@users.noreply.github.com>
2019-04-10 19:07:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cardsagainsthumanity/Main.java
@@ -0,0 +1,10 @@
+package com.example.cardsagainsthumanity;
+
+public class Main {
+    public static void main(String[] args) {
+        Player p1 = new Player();
+        System.out.println(p1);
+        Player p2 = new Player("pepito");
+        System.out.println(p2);
+    }
+}
---
+++ app/src/main/java/com/example/cardsagainsthumanity/Player.java
@@ -0,0 +1,43 @@
+package com.example.cardsagainsthumanity;
+
+public class Player {
+
+    private String name = "Default Name";
+    private int nCards = 0;
+    private boolean myTurn = false;
+    private int score = 0;
+    private boolean master = false;
+
+    public Player (){ }
+
+    //overload
+    public Player(String name){
+        this.name = name;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getnCards() {
+        return nCards;
+    }
+
+    public boolean isMyTurn() {
+        return myTurn;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public boolean isMaster() {
+        return master;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: "+name+"\nNumber of cards: "+nCards+"\nScore: "+score;
+    }
+}
