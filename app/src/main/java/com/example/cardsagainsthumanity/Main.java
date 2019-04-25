bc2f47d4d6210dace8f6fda56a463743b12976f4
cesar <cesyser@gmail.com>
2019-04-25 11:32:05 +0200

    PlayersCraft added

--- app/src/main/java/com/example/cardsagainsthumanity/Main.java
+++ app/src/main/java/com/example/cardsagainsthumanity/Main.java
@@ -6,14 +6,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-
-        playerCraft(4,teclado);
+        int nPlayers = 0;
+        do{
+            System.out.println("¿Cuántos jugadores van a jugar? \nMáximo de 8 jugadores, mínimo 3.");
+            nPlayers = Integer.parseInt(teclado.nextLine());
+        }while(nPlayers < 3 || nPlayers > 8);
+        playerCraft(nPlayers,teclado);
     }
 
     public static void playerCraft(int nPlayers, Scanner teclado){
         Player [] players = new Player[nPlayers];
         for (int i = 0; i < nPlayers; i++) {
-            System.out.println("Nombre jugador "+(i+1)+": ");
+            System.out.print("Nombre jugador "+(i+1)+": ");
             String name = teclado.nextLine();
             if(name.equals("")){
                players[i] = new Player();
