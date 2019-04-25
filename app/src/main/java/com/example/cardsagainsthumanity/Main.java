dab22e6f02dbf887376682527e4ab74270954abf
cesar <cesyser@gmail.com>
2019-04-25 11:22:46 +0200

    PlayersCraft added

--- app/src/main/java/com/example/cardsagainsthumanity/Main.java
+++ app/src/main/java/com/example/cardsagainsthumanity/Main.java
@@ -1,27 +1,28 @@
 package com.example.cardsagainsthumanity;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Player> playerList = new ArrayList<Player>();
+        Scanner teclado = new Scanner(System.in);
 
-        Player p1 = new Player();
-        playerList.add(p1);
-        p1.setTurn(playerList.size());
-        System.out.println(p1);
-        System.out.println(p1.getTurn());
+        playerCraft(4,teclado);
+    }
 
-        Player p2 = new Player("César");
-        playerList.add(p2);
-        p2.setTurn(playerList.size());
-        System.out.println(p2);
-        System.out.println(p2.getTurn());
+    public static void playerCraft(int nPlayers, Scanner teclado){
+        Player [] players = new Player[nPlayers];
+        for (int i = 0; i < nPlayers; i++) {
+            System.out.println("Nombre jugador "+(i+1)+": ");
+            String name = teclado.nextLine();
+            if(name.equals("")){
+               players[i] = new Player();
+            }else{
+                players[i] = new Player(name);
+            }
+            players[i].setTurn(i);
 
-        Player p3 = new Player();
-        playerList.add(p3);
-        p3.setTurn(playerList.size());
-        System.out.println(p3);
-        System.out.println(p3.getTurn());
+            System.out.println(players[i]);
+        }
     }
 }
